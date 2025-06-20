0520491c4a4e3005abac4340d26ec1420b19e666
Eftychios Karagiorgis <eftichios1997@hotmail.com>
2025-06-20 17:15:02 +0300

    Change color scheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,22 @@
+require("tokyonight").setup({
+    style = "storm",
+
+    on_colors = function(colors)
+        colors.comment = "#78749a"
+    end
+})
+
 function ColorMyPencils(color)
-    color = color or "rose-pine"
+    color = color or "tokyonight-storm"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+    -- line numbers
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#70826f"})
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#8ce386"})
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#70826f"})
 
 end
 
